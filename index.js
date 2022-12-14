f5fb9ab53de18ed54061c61e8e9ffd0422898d37
Fazekas Adrienn <adreeja17@gmail.com>
2022-12-14 17:49:38 +0100

    using inner.HTML

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 
-let myLeads = []
+let myLeads = ["www.croissant.com", "www.baguette.com"]
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
 
 inputBtn.addEventListener("click", function() {
     myLeads.push(inputEl.value)
     console.log(myLeads)
 })
+
+for (let i = 0; i < myLeads.length; i++) {
+    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+}
